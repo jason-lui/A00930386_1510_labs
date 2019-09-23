ad91230cd5b168bf8bed998db69605a0eba7f552
Jason Lui <mrjasonlui@gmail.com>
2019-09-23 14:21:31 -0700

    Added helper function

--- Lab03/base_conversion.py
+++ Lab03/base_conversion.py
@@ -9,12 +9,21 @@ def base_conversion():
     print(f'The maximum decimal number that will fit within 4 bits in base {base} is {max_base_ten}.')
     num = float(input(f'Enter a positive number less than {max_base_ten} to be converted: '))
 
-    first_q, first_r = num // 2, int(num % 2)
-    second_q, second_r = first_q // 2, int(first_q % 2)
-    third_q, third_r = second_q // 2, int(second_q % 2)
-    fourth_q, fourth_r = third_q // 2, int(third_q % 2)
+    (first_q, first_r) = quotient_remainder_division(num, base)
+    (second_q, second_r) = quotient_remainder_division(first_q, base)
+    (third_q, third_r) = quotient_remainder_division(second_q, base)
+    (fourth_q, fourth_r) = quotient_remainder_division(third_q, base)
 
     res = int(str(fourth_r) + str(third_r) + str(second_r) + str(first_r))
 
     print(f'The decimal number {num} in base {base} is {res}.')
     return
+
+
+def quotient_remainder_division(number, divisor):
+    quotient = int(number // divisor)
+    remainder = int(number % divisor)
+    return (quotient, remainder)
+
+
+base_conversion()
