a9210342c08c4617a2dd14cf4bdeb6a375c8397f
Jason Lui <mrjasonlui@gmail.com>
2019-09-23 14:42:12 -0700

    Edited helper function to make it cleaner

--- Lab03/base_conversion.py
+++ Lab03/base_conversion.py
@@ -7,23 +7,34 @@ def base_conversion():
     base = int(input('Enter the destination base (2-9): '))
     max_base_ten = base ** 4 - 1
     print(f'The maximum decimal number that will fit within 4 bits in base {base} is {max_base_ten}.')
-    num = float(input(f'Enter a positive number less than {max_base_ten} to be converted: '))
+    num = int(input(f'Enter a positive number less than {max_base_ten} to be converted: '))
+    res = ""
 
-    (first_q, first_r) = quotient_remainder_division(num, base)
-    (second_q, second_r) = quotient_remainder_division(first_q, base)
-    (third_q, third_r) = quotient_remainder_division(second_q, base)
-    (fourth_q, fourth_r) = quotient_remainder_division(third_q, base)
+    quotient = num
+    res = string_remainder(quotient, base) + res
+    quotient = quotient // 2
+    res = string_remainder(quotient, base) + res
+    quotient = quotient // 2
+    res = string_remainder(quotient, base) + res
+    quotient = quotient // 2
+    res = string_remainder(quotient, base) + res
 
-    res = int(str(fourth_r) + str(third_r) + str(second_r) + str(first_r))
+    res = int(res)
 
     print(f'The decimal number {num} in base {base} is {res}.')
     return
 
+def string_remainder(quotient, base):
+    """
+    Returns the remainder as a string.
 
-def quotient_remainder_division(number, divisor):
-    quotient = int(number // divisor)
-    remainder = int(number % divisor)
-    return (quotient, remainder)
-
+    :param quotient: an integer
+    :param base: an integer
+    :precondition: quotient must be an integer
+    :precondition: base must be an integer
+    :postcondition: generates the remainder as a string
+    :return: the remainder as a string
+    """
+    return str(int(quotient) % 2)
 
 base_conversion()
