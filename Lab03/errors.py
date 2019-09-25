9b4f1820a95445c57b5b4dd30a67d94d101e21e8
Jason Lui <mrjasonlui@gmail.com>
2019-09-25 12:05:52 -0700

    Finished main function

--- Lab03/errors.py
+++ Lab03/errors.py
@@ -1,3 +1,28 @@
 def main():
-    ZeroDivisionError = 10 / 0
-    return
+    """
+    Drive the program.
+    """
+    # ZeroDivisionError
+    integer = 10
+    zero_error_1 = integer / 0
+    zero_error_2 = integer % 0
+
+    # IndexError
+    string = "abc"
+    index_error_1 = string[10]
+    index_error_2 = string[-10]
+
+    # TypeError
+    letter = "a"
+    type_error_1 = string + integer
+    type_error_2 = zero[-10]
+
+
+# Call the main function
+if __name__ == '__main__':
+    main()
+
+
+# Call the main function
+if __name__ == '__main__':
+    main()
