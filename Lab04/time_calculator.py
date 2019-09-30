89017f3aca87ef167b13a068926e6618d124abf6
Jason Lui <mrjasonlui@gmail.com>
2019-09-30 16:29:27 -0700

    Added additional docstring tests for time_calculator

--- Lab04/time_calculator.py
+++ Lab04/time_calculator.py
@@ -11,42 +11,62 @@ def time_calculator(seconds):
 
     >>> time_calculator(0)
     0 0 0 0
+    >>> time_calculator(60)
+    0 0 1 0
+    >>> time_calculator(3600)
+    0 1 0 0
+    >>> time_calculator(86400)
+    1 0 0 0
     >>> time_calculator(100000)
     1 3 46 40
     """
-    # List of the desired time frames and their values in seconds
-    seconds_per_day = 86400
-    seconds_per_hour = 3600
-    seconds_per_minute = 60
-
     # Separate desired time frames into variables
-    days = time_converter(seconds, seconds_per_day)
-    seconds %= seconds_per_day
-    hours = time_converter(seconds, seconds_per_hour)
-    seconds %= seconds_per_hour
-    minutes = time_converter(seconds, seconds_per_minute)
-    seconds %= seconds_per_minute
+    days = time_converter(seconds, "days")
+    hours = time_converter(seconds, "hours")
+    minutes = time_converter(seconds, "minutes")
+    seconds = time_converter(seconds, "seconds")
 
     print(days, hours, minutes, seconds)
     return
 
 
-def time_converter(seconds, time_frame):
+def time_converter(dividend, divisor):
     """
-    Convert seconds to the specified time frame.
+    Returns the remainder
 
-    :param seconds: an integer
-    :param time_frame: an integer
-    :precondition: time_frame must be an integer
-    :postcondition: the returned amount of time is an integer
-    :return: the seconds divided by the time_frame as an integer
+    :param divisor: an integer
+    :param dividend: an integer
+    :precondition: divisor must be an integer
+    :postcondition: calculate the integer division
+    :return: the number of times the divisor can divide the dividend
 
-    >>> time_converter(3000, 3600)
+    >>> time_converter(3000, "hours")
     0
-    >>> time_converter(4000, 3600)
+    >>> time_converter(4000, "hours")
     1
+    >>> time_converter(38000, "hours")
+    10
     """
-    return seconds // time_frame
+    # Variables of different time frames in seconds
+    seconds_per_day = 86400
+    seconds_per_hour = 3600
+    seconds_per_minute = 60
+    second = 1
+
+    if divisor == "days":
+        dividend //= seconds_per_day
+    if divisor == "hours":
+        dividend %= seconds_per_day
+        dividend //= seconds_per_hour
+    if divisor == "minutes":
+        dividend %= seconds_per_hour
+        dividend //= seconds_per_minute
+    if divisor == "seconds":
+        dividend %= seconds_per_minute
+
+    return dividend
+
+
 
 
 def main():
@@ -64,7 +84,7 @@ if __name__ == "__main__":
 Component(s) of computational thinking
 
 Decomposition
-time_calculator() uses the helper function time_converter() to calculate the
+time_calculator() uses the helper function integer_division() to calculate the
 number of days/hours/minutes given a number of seconds. The helper function
 can be implemented into any program that requires a conversion of time if given seconds.
 """
