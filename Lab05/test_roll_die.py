2e5f91133e9f9da3df3148eedc6801bd66b68d41
Jason Lui <mrjasonlui@gmail.com>
2019-10-08 12:17:47 -0700

    Moved patch statements

--- Lab05/test_roll_die.py
+++ Lab05/test_roll_die.py
@@ -3,24 +3,27 @@ from unittest.mock import patch
 from lab05 import roll_die
 
 
-@patch("random.randint", side_effect = [1, 2, 3, 4, 5, 6])
 class TestRoll_die(TestCase):
 
+    @patch("random.randint", side_effect=[1, 2, 3, 4, 5, 6])
     def test_roll_0_0(self, mock_roll):
         number_of_rolls = 0
         number_of_sides = 0
         self.assertEqual(0, roll_die(number_of_rolls, number_of_sides))
 
+    @patch("random.randint", side_effect=[1, 2, 3, 4, 5, 6])
     def test_roll_6_0(self, mock_roll):
         number_of_rolls = 6
         number_of_sides = 0
         self.assertEqual(0, roll_die(number_of_rolls, number_of_sides))
 
+    @patch("random.randint", side_effect=[1, 2, 3, 4, 5, 6])
     def test_roll_0_6(self, mock_roll):
         number_of_rolls = 0
         number_of_sides = 6
         self.assertEqual(0, roll_die(number_of_rolls, number_of_sides))
 
+    @patch("random.randint", side_effect=[1, 2, 3, 4, 5, 6])
     def test_roll_6_6(self, mock_roll):
         number_of_rolls = 6
         number_of_sides = 6
