558fb20e46d95941858e256aa5661ceef21a06ee
Jason Lui <mrjasonlui@gmail.com>
2019-10-09 14:58:33 -0700

    Finished 6 vowel tests

--- Lab05/test_generate_vowel.py
+++ Lab05/test_generate_vowel.py
@@ -1,6 +1,30 @@
 from unittest import TestCase
+from unittest.mock import patch
+from lab05 import generate_vowel
 
 
 class TestGenerate_vowel(TestCase):
-    def test_generate_vowel(self):
-        self.fail()
+
+    @patch("random.choice", return_value=97)
+    def test_generate_vowel_a(self, mock_output):
+        self.assertEqual("a", generate_vowel())
+
+    @patch("random.choice", return_value=101)
+    def test_generate_vowel_e(self, mock_output):
+        self.assertEqual("e", generate_vowel())
+
+    @patch("random.choice", return_value=105)
+    def test_generate_vowel_i(self, mock_output):
+        self.assertEqual("i", generate_vowel())
+
+    @patch("random.choice", return_value=111)
+    def test_generate_vowel_o(self, mock_output):
+        self.assertEqual("o", generate_vowel())
+
+    @patch("random.choice", return_value=117)
+    def test_generate_vowel_u(self, mock_output):
+        self.assertEqual("u", generate_vowel())
+
+    @patch("random.choice", return_value=121)
+    def test_generate_vowel_y(self, mock_output):
+        self.assertEqual("y", generate_vowel())
