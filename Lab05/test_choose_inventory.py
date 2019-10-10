531d9e75cdfdfcf396760a17e6be7add25c6b89d
Jason Lui <mrjasonlui@gmail.com>
2019-10-10 11:47:37 -0700

    Split assertions

--- Lab05/test_choose_inventory.py
+++ Lab05/test_choose_inventory.py
@@ -6,15 +6,17 @@ import io
 
 class TestChoose_inventory(TestCase):
 
+    @patch("sys.stdout", new_callable=io.StringIO)
     @patch("random.sample", side_effect=[[], [], [], []])
-    def test_choose_inventory_0_0(self, mock_items):
+    def test_choose_inventory_0_0(self, mock_items, mock_output):
         inventory = []
         selection = 0
         expected = []
         self.assertEqual(expected, choose_inventory(inventory, selection))
 
+    @patch("sys.stdout", new_callable=io.StringIO)
     @patch("random.sample", side_effect=[[], [], [], []])
-    def test_choose_inventory_4_0(self, mock_items):
+    def test_choose_inventory_4_0(self, mock_items, mock_output):
         inventory = ['Rabadon\'s Deathcap', 'Abyssal\' Mask', 'Rapidfire Cannon', 'Boots of Lucidity']
         selection = 0
         expected = []
@@ -22,13 +24,20 @@ class TestChoose_inventory(TestCase):
 
     @patch("sys.stdout", new_callable=io.StringIO)
     @patch("random.sample", side_effect=[[], [], [], []])
-    def test_choose_inventory_0_n1(self, mock_items, mock_output):
+    def test_choose_inventory_0_n1_list(self, mock_items, mock_output):
         inventory = []
         selection = -1
         expected = []
-        expected_string = "WARNING: The number of items selected is negative.\n"
         self.assertEqual(expected, choose_inventory(inventory, selection))
-        self.assertEqual(expected_string, mock_output.getvalue())
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("random.sample", side_effect=[[], [], [], []])
+    def test_choose_inventory_0_n1_print(self, mock_items, mock_output):
+        inventory = []
+        selection = -1
+        expected = "WARNING: The number of items selected is negative.\n"
+        choose_inventory(inventory, selection)
+        self.assertEqual(expected, mock_output.getvalue())
 
     @patch("sys.stdout", new_callable=io.StringIO)
     @patch("random.sample", side_effect=[[], [], [], []])
@@ -36,9 +45,16 @@ class TestChoose_inventory(TestCase):
         inventory = ['Rabadon\'s Deathcap', 'Abyssal\' Mask', 'Rapidfire Cannon', 'Boots of Lucidity']
         selection = -1
         expected = []
-        expected_string = "WARNING: The number of items selected is negative.\n"
         self.assertEqual(expected, choose_inventory(inventory, selection))
-        self.assertEqual(expected_string, mock_output.getvalue())
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("random.sample", side_effect=[[], [], [], []])
+    def test_choose_inventory_4_n1_print(self, mock_items, mock_output):
+        inventory = ['Rabadon\'s Deathcap', 'Abyssal\' Mask', 'Rapidfire Cannon', 'Boots of Lucidity']
+        selection = -1
+        expected = "WARNING: The number of items selected is negative.\n"
+        choose_inventory(inventory, selection)
+        self.assertEqual(expected, mock_output.getvalue())
 
     @patch("sys.stdout", new_callable=io.StringIO)
     @patch("random.sample", side_effect=[[], [], [], []])
@@ -46,12 +62,20 @@ class TestChoose_inventory(TestCase):
         inventory = []
         selection = 4
         expected = []
-        expected_string = "WARNING: The number of items selected is larger than the size of the inventory.\n"
         self.assertEqual(expected, choose_inventory(inventory, selection))
-        self.assertEqual(expected_string, mock_output.getvalue())
 
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("random.sample", side_effect=[[], [], [], []])
+    def test_choose_inventory_0_4_print(self, mock_items, mock_output):
+        inventory = []
+        selection = 4
+        expected = "WARNING: The number of items selected is larger than the size of the inventory.\n"
+        choose_inventory(inventory, selection)
+        self.assertEqual(expected, mock_output.getvalue())
+
+    @patch("sys.stdout", new_callable=io.StringIO)
     @patch("random.sample", side_effect=[['Rapidfire Cannon', 'Boots of Lucidity']])
-    def test_choose_inventory_4_2(self, mock_items):
+    def test_choose_inventory_4_2(self, mock_items, mock_output):
         inventory = ['Rabadon\'s Deathcap', 'Abyssal\' Mask', 'Rapidfire Cannon', 'Boots of Lucidity']
         selection = 2
         self.assertEqual(['Boots of Lucidity', 'Rapidfire Cannon'], choose_inventory(inventory, selection))
