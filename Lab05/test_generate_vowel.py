27cb4fc513b092b79cbb5f6fb452afd2f8936a69
Jason Lui <mrjasonlui@gmail.com>
2019-10-10 10:17:19 -0700

    Changed formatting of tests

--- Lab05/test_generate_vowel.py
+++ Lab05/test_generate_vowel.py
@@ -7,24 +7,30 @@ class TestGenerate_vowel(TestCase):
 
     @patch("random.choice", return_value=97)
     def test_generate_vowel_a(self, mock_output):
-        self.assertEqual("a", generate_vowel())
+        expected_letter = "a"
+        self.assertEqual(expected_letter, generate_vowel())
 
     @patch("random.choice", return_value=101)
     def test_generate_vowel_e(self, mock_output):
-        self.assertEqual("e", generate_vowel())
+        expected_letter = "e"
+        self.assertEqual(expected_letter, generate_vowel())
 
     @patch("random.choice", return_value=105)
     def test_generate_vowel_i(self, mock_output):
-        self.assertEqual("i", generate_vowel())
+        expected_letter = "i"
+        self.assertEqual(expected_letter, generate_vowel())
 
     @patch("random.choice", return_value=111)
     def test_generate_vowel_o(self, mock_output):
-        self.assertEqual("o", generate_vowel())
+        expected_letter = "o"
+        self.assertEqual(expected_letter, generate_vowel())
 
     @patch("random.choice", return_value=117)
     def test_generate_vowel_u(self, mock_output):
-        self.assertEqual("u", generate_vowel())
+        expected_letter = "u"
+        self.assertEqual(expected_letter, generate_vowel())
 
     @patch("random.choice", return_value=121)
     def test_generate_vowel_y(self, mock_output):
-        self.assertEqual("y", generate_vowel())
+        expected_letter = "y"
+        self.assertEqual(expected_letter, generate_vowel())
