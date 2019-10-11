fac37fd90c781a5771e6d771750b322c6e729980
Jason Lui <mrjasonlui@gmail.com>
2019-10-10 21:53:53 -0700

    Added unit test

--- Lab05/test_generate_vowel.py
+++ Lab05/test_generate_vowel.py
@@ -6,5 +6,10 @@ vowels = ["a", "e", "i", "o", "u", "y"]
 
 class TestGenerate_vowel(TestCase):
 
+    # Make sure the generated string is a vowel
     def test_generate_vowel(self):
         self.assertTrue(generate_vowel() in vowels)
+
+    # Make sure the generated string is a single character
+    def test_generate_vowel_len(self):
+        self.assertEqual(1, len(generate_vowel()))
