7494ade95d7a150ebef54cf5ee122fdec91c68cb
Jason Lui <mrjasonlui@gmail.com>
2019-10-17 14:22:59 -0700

    Added unit tests Made vectors more sparse

--- Lab06/test_sparse_add.py
+++ Lab06/test_sparse_add.py
@@ -10,20 +10,38 @@ class TestSparse_add(TestCase):
         expected = {}
         self.assertEqual(expected, sparse_add(v1, v2))
 
+    def test_sparse_add_same_indices(self):
+        v1 = {1: 1, 2: 2, 99: 3}
+        v2 = {1: 2, 2: 4, 99: 6}
+        expected = {1: 3, 2: 6, 99: 9}
+        self.assertEqual(expected, sparse_add(v1, v2))
+
     def test_sparse_add_v1_empty(self):
         v1 = {}
-        v2 = {0: 1, 1: 2, 2: 3, 3: 4}
-        expected = {0: 1, 1: 2, 2: 3, 3: 4}
+        v2 = {0: 1, 1: 2, 2: 3, 99: 4}
+        expected = {0: 1, 1: 2, 2: 3, 99: 4}
         self.assertEqual(expected, sparse_add(v1, v2))
 
     def test_sparse_add_v2_empty(self):
-        v1 = {0: 1, 1: 2, 2: 3, 3: 4}
+        v1 = {0: 1, 1: 2, 2: 3, 99: 4}
         v2 = {}
-        expected = {0: 1, 1: 2, 2: 3, 3: 4}
+        expected = {0: 1, 1: 2, 2: 3, 99: 4}
         self.assertEqual(expected, sparse_add(v1, v2))
 
-    def test_sparse_add(self):
-        v1 = {0: 1, 1: 2, 2: 3, 3: 4}
-        v2 = {2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6}
-        expected = {0: 1, 1: 2, 2: 4, 3: 6, 4: 3, 5: 4, 6: 5, 7: 6}
+    def test_sparse_no_matching_indices(self):
+        v1 = {1: 1, 2: 2, 98: 3}
+        v2 = {4: 4, 5: 5, 99: 6}
+        expected = {1: 1, 2: 2, 4: 4, 5: 5, 98: 3, 99: 6}
+        self.assertEqual(expected, sparse_add(v1, v2))
+
+    def test_sparse_some_matching_indices(self):
+        v1 = {1: 1, 2: 2, 99: 3}
+        v2 = {2: 2, 3: 3, 99: 4}
+        expected = {1: 1, 2: 4, 3: 3, 99: 7}
+        self.assertEqual(expected, sparse_add(v1, v2))
+
+    def test_sparse_add_to_zero(self):
+        v1 = {1: -5, 3: -3, 99: -1}
+        v2 = {1: 5, 3: 3, 99: 1}
+        expected = {1: 0, 3: 0, 99: 0}
         self.assertEqual(expected, sparse_add(v1, v2))
