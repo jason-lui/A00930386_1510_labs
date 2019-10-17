a98f33f1d1982b6af065f76abf225255f3f494fc
Jason Lui <mrjasonlui@gmail.com>
2019-10-16 23:03:12 -0700

    Edited sparse_add Added doctests

--- Lab06/sparsevector.py
+++ Lab06/sparsevector.py
@@ -1,9 +1,13 @@
+import doctest
+
+
 """
 We cannot return the length of a sparse vector that is formatted as a dictionary.
 The dictionary contains only non-zero values, so it does not show how many zeroes there were
 in the original sparse vector. We need to ask the team lead to create a new dictionary entry
 that represents the total number of zeroes in the original sparse vector.
 """
+
 
 def sparse_add(vector_1, vector_2):
     """
@@ -15,11 +19,27 @@ def sparse_add(vector_1, vector_2):
     :precondition: vector_2 must be a list of integers
     :postcondition: the vectors will be added together
     :return: a dictionary representing the combined vectors.
+
+    >>> sparse_add({}, {0:1, 2:2, 4:3})
+    {0: 1, 2: 2, 4: 3}
+    >>> sparse_add({0:1, 2:2, 4:3}, {0:1, 1:3, 2:2, 3:3, 4:3, 5:9})
+    {0: 2, 1: 3, 2: 4, 3: 3, 4: 6, 5: 9}
     """
+    new_dict = {}
+
+    # Put all entries into vector_1
     for key in vector_2.keys():
         if key in vector_1:
             vector_1[key] += vector_2[key]
         else:
             vector_1[key] = vector_2[key]
 
-    return vector_1
+    # Reorder the entries in memory
+    for key in sorted(vector_1.keys()):
+        new_dict[key] = vector_1[key]
+
+    return new_dict
+
+
+if __name__ == '__main__':
+    doctest.testmod()
