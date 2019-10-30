13269afcf228e4669a0bc7b19a6fbcf69c0d8459
Jason Lui <mrjasonlui@gmail.com>
2019-10-30 15:26:43 -0700

    Finished unit tests for cutoff

--- Lab07/test_cutoff.py
+++ Lab07/test_cutoff.py
@@ -1,6 +1,77 @@
 from unittest import TestCase
+from midterm import cutoff
 
 
 class TestCutoff(TestCase):
-    def test_cutoff(self):
-        self.fail()
+
+    def test_cutoff_empty_0(self):
+        test_list = []
+        test_cutoff = 0
+        expected = 0
+        self.assertEqual(expected, cutoff(test_list, test_cutoff))
+
+    def test_cutoff_empty_list_5(self):
+        test_list = []
+        test_cutoff = 5
+        expected = 0
+        self.assertEqual(expected, cutoff(test_list, test_cutoff))
+
+    def test_cutoff_list_0_and_0(self):
+        test_list = [0]
+        test_cutoff = 0
+        expected = 0
+        self.assertEqual(expected, cutoff(test_list, test_cutoff))
+
+    def test_cutoff_list_0_and_5(self):
+        test_list = [0]
+        test_cutoff = 5
+        expected = 0
+        self.assertEqual(expected, cutoff(test_list, test_cutoff))
+
+    def test_cutoff_list_2_and_2(self):
+        test_list = [2]
+        test_cutoff = 2
+        expected = 1
+        self.assertEqual(expected, cutoff(test_list, test_cutoff))
+
+    def test_cutoff_list_2_and_4(self):
+        test_list = [2]
+        test_cutoff = 4
+        expected = 0
+        self.assertEqual(expected, cutoff(test_list, test_cutoff))
+
+    def test_cutoff_list_5_pos_and_0(self):
+        test_list = [1, 2, 3, 4, 5]
+        test_cutoff = 0
+        expected = 0
+        self.assertEqual(expected, cutoff(test_list, test_cutoff))
+
+    def test_cutoff_list_5_pos_and_not_0(self):
+        test_list = [1, 2, 3, 4, 5]
+        test_cutoff = 3
+        expected = 1
+        self.assertEqual(expected, cutoff(test_list, test_cutoff))
+
+    def test_cutoff_list_5x2_and_2(self):
+        test_list = [2, 2, 2, 2, 2]
+        test_cutoff = 2
+        expected = 5
+        self.assertEqual(expected, cutoff(test_list, test_cutoff))
+
+    def test_cutoff_list_5x2_and_3(self):
+        test_list = [2, 2, 2, 2, 2]
+        test_cutoff = 3
+        expected = 0
+        self.assertEqual(expected, cutoff(test_list, test_cutoff))
+
+    def test_cutoff_list_all_divisible(self):
+        test_list = [3, 6, 9, 12, 15]
+        test_cutoff = 3
+        expected = 5
+        self.assertEqual(expected, cutoff(test_list, test_cutoff))
+
+    def test_cutoff_negatives(self):
+        test_list = [-1, -2, -3, -4, -5, -6]
+        test_cutoff = -1
+        expected = 1
+        self.assertEqual(expected, cutoff(test_list, test_cutoff))
