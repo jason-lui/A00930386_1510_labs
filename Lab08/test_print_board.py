f73724c9759e2c838b5c8ce4a41ede18fb2d9de8
Jason Lui <mrjasonlui@gmail.com>
2019-11-05 12:11:08 -0800

    Finished unit tests

--- Lab08/test_print_board.py
+++ Lab08/test_print_board.py
@@ -6,31 +6,6 @@ import io
 
 class TestPrint_board(TestCase):
 
-    # def print_board(board: list):
-    #     """
-    #     Print the board.
-    #
-    #     :param board: a list of lists
-    #     :precondition: board must be a list of lists of int representing the board
-    #     :postcondition: the board will be printed as a 2D array
-    #
-    #     >>> print_board([[1,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,2]])
-    #     1 0 0 0 0
-    #     0 0 0 0 0
-    #     0 0 0 0 0
-    #     0 0 0 0 0
-    #     0 0 0 0 2
-    #     >>> print_board([[1,0,0], [0,0,0], [0,0,2]])
-    #     1 0 0
-    #     0 0 0
-    #     0 0 2
-    #     """
-    #     for row in board:
-    #         row_str = ""
-    #         for cell in row:
-    #             row_str += str(cell) + ' '
-    #         print(row_str.strip())
-
     @patch("sys.stdout", new_callable=io.StringIO)
     def test_print_board_start_0_0_end_4_4_in_5x5_board(self, mock_output):
         test_board = [[1, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 2]]
