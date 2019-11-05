ccf09819041faab943e55ebe0b73d5edb8c0e123
Jason Lui <mrjasonlui@gmail.com>
2019-11-05 11:45:58 -0800

    Finished unit test

--- Lab08/test_make_board.py
+++ Lab08/test_make_board.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
+from maze import make_board
 
 
 class TestMake_board(TestCase):
+
     def test_make_board(self):
-        self.fail()
+        expected = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 2]]
+        self.assertEqual(expected, make_board())
