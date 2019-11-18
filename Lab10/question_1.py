1e7e02a2f3053c512c9d35c46675ea35d0654627
Jason Lui <mrjasonlui@gmail.com>
2019-11-18 11:02:57 -0800

    Finished doctests

--- Lab10/question_1.py
+++ Lab10/question_1.py
@@ -1,19 +1,35 @@
-def eratosthenes(n: int) -> int:
-    """
+import doctest
 
-    :param n:
-    :return:
-    """
-    if n <= 0:
-        print("Enter a positive integer.")
-        return
 
+def eratosthenes(n: int) -> list:
+    """
+    Determine all the prime numbers up to n.
+
+    :param n: a positive integer
+    :precondition: n must be a positive integer
+    :postcondition: all primes will be returned
+    :return: a list of all the primes up to n
+
+    >>> eratosthenes(-1)
+    Traceback (most recent call last):
+      ...
+    Exception: n should be a positive integer. The value of n was: -1.
+    >>> eratosthenes(1)
+    []
+    >>> eratosthenes(2)
+    [2]
+    >>> eratosthenes(30)
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    """
     # 0 and 1 are not primes. 2 is prime.
     # Total number of entries is n + 1, so n + 1 - 2 = n - 1
+    if n <= 0:
+        raise Exception(f"n should be a positive integer. The value of n was: {n}.")
+
     primes = [False, False] + [True] * (n - 1)
+
     for i in range(2, int(n ** 0.5) + 1):
         if primes[i]:  # Number is prime
-
             # Sieve the multiples of i
             for j in range(i * 2, n + 1, i):
                 primes[j] = False
@@ -21,4 +37,5 @@ def eratosthenes(n: int) -> int:
     return [i for i in range(n + 1) if primes[i]]
 
 
-print(eratosthenes(30))
+if __name__ == '__main__':
+    doctest.testmod()
