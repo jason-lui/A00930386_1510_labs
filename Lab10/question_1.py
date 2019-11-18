e47e092cab27e51cd53c2b925ef28f1de22f02a7
Jason Lui <mrjasonlui@gmail.com>
2019-11-18 11:47:55 -0800

    Changed comments

--- Lab10/question_1.py
+++ Lab10/question_1.py
@@ -21,11 +21,12 @@ def eratosthenes(n: int) -> list:
     >>> eratosthenes(30)
     [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
     """
-    # 0 and 1 are not primes. 2 is prime.
-    # Total number of entries is n + 1, so n + 1 - 2 = n - 1
+    # Raise exception if n is not a positive integer
     if n <= 0:
         raise Exception(f"n should be a positive integer. The value of n was: {n}.")
 
+    # 0 and 1 are not primes. 2 is prime.
+    # Total number of entries is n + 1, so n + 1 - 2 = n - 1
     primes = [False, False] + [True] * (n - 1)
 
     for i in range(2, int(n ** 0.5) + 1):
