3a671be73e8c42d3a9b218566d96f97fc11487d7
Jason Lui <mrjasonlui@gmail.com>
2019-11-18 11:25:12 -0800

    Deleted question_2 to question_8 because they're for Assignment 4

--- Lab10/question_6.py
+++
@@ -1,9 +0,0 @@
-"""
-Test cases
-#1) Lines do not intersect
-#2) Lines are coincident
-#3) Lines intersect and both have negative slopes
-#4) Lines intersect and both have positive slopes
-#5) Lines intersect and one has a negative slope while the other has a positive slope
-#6) Lines intersect and one line has 0 slope and the other is vertical
-"""
